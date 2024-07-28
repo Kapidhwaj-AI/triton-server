e640a117604f9f664a31e2897bf6d1601797991f
Abdur Nawaz <abdurnawaz.2011@gmail.com>
2024-07-28 20:00:54 +0530

    added dlib model

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,9 @@ RUN pip install opencv-python && \
     apt install -y libgl1 && \
     rm -rf /var/lib/apt/lists/*
 
+RUN apt-get update -y && \
+    apt-get install build-essential cmake pkg-config
+
+RUN pip install face_recognition==1.3.0
+
 CMD ["tritonserver", "--model-repository=/models"]
---
+++ models/dlib/1/model.py
@@ -0,0 +1,63 @@
+import json
+
+import dlib
+import face_recognition_models
+from face_recognition.api import _raw_face_landmarks
+import numpy as np
+import triton_python_backend_utils as pb_utils
+
+
+class TritonPythonModel:
+
+    def initialize(self, args):
+        
+        self.model_config = model_config = json.loads(args["model_config"])
+
+        face_recognition_model = face_recognition_models.face_recognition_model_location()
+        self.face_encoder = dlib.face_recognition_model_v1(face_recognition_model)
+
+        embeddings_config = pb_utils.get_output_config_by_name(
+            model_config, "embeddings"
+        )
+
+        self.embeddings_dtype = pb_utils.triton_string_to_numpy(
+            embeddings_config["data_type"]
+        )
+
+
+    def execute(self, requests):
+
+        outputs = []
+
+        for request in requests:
+
+            image_tensor = pb_utils.get_input_tensor_by_name(request, "image")
+            face_locations_tensor = pb_utils.get_input_tensor_by_name(request, "face_locations")
+            frame = image_tensor.as_numpy()
+            face_locations = face_locations_tensor.as_numpy()
+
+            embeddings = self.get_batch_encodings(frame, face_locations)
+            outputs.append(embeddings)
+
+        embeddings_tensor = pb_utils.Tensor(
+            "embeddings", np.array(outputs).astype(self.embeddings_dtype)
+        )
+        inference_response = pb_utils.InferenceResponse(
+            output_tensors=[
+                embeddings_tensor
+            ]
+        )
+
+        return inference_response
+    
+
+    def get_batch_encodings(self, face_image, known_face_locations, num_jitters=1, model="small"):
+        try:
+            dlib_vector = dlib.full_object_detections()
+            raw_landmarks = _raw_face_landmarks(face_image, known_face_locations, model)
+            dlib_vector.extend(raw_landmarks)
+            embeddings = np.array(self.face_encoder.compute_face_descriptor(face_image, dlib_vector, num_jitters))
+            return embeddings
+        except Exception as e:
+            print(e)
+            return np.array([])
